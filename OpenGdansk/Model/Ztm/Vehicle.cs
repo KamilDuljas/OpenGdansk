23e9bd7e5fd7297743f4baa3c7fdb7fb2a2ebc99
Kamil Duljas <kamil.duljas@gmail.com>
2025-04-30 13:37:44 +0200

    Add preliminary model

---
+++ OpenGdansk/Model/Ztm/Vehicle.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.WebRequestMethods;
+
+namespace OpenGdansk.Model.Ztm
+{
+    public class Header
+    {
+        public const string URL_HEADER = "https://files.cloudgdansk.pl/d/otwarte-dane/ztm/baza-pojazdow.header.json?v=2";
+
+        public string Version { get; set; }
+        public string Title { get; set; }
+        public DateTime SourceDate {  get; set; }
+        public DateTime GenerationDate {  get; set; }
+        public string ApiUrlData { get; set; }
+
+    }
+
+    public class Vehicle
+    {
+        public const string URL_PHOTO = "https://files.cloudgdansk.pl/f/otwarte-dane/ztm/baza-pojazdow/";
+    }
+
+}
---
+++ OpenGdansk/Services/DataService.cs
@@ -0,0 +1,30 @@
+﻿using OpenGdansk.Model.Ztm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace OpenGdansk.Services
+{
+    public class DataService
+    {
+        private readonly HttpClient _httpClient;
+
+        public DataService()
+        {
+            _httpClient = new HttpClient();
+        }
+
+        public async Task<Header> GetHeaderAsync(string url)
+        {
+              var response = await _httpClient.GetStringAsync(url);
+            return response == null
+                ? throw new InvalidOperationException("Response from GetHeaderAsync is null!")
+                : JsonSerializer.Deserialize<Header>(response)!;
+        }
+
+    }
+}
